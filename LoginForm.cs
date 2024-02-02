7db4f5fa814cb0f4533726a934c31ea777ded8d5
akram <akram7.dris7@gmail.com>
2024-02-02 20:51:54 +0100

    add validation of text box max length and empty Error

--- LoginForm.cs
+++ LoginForm.cs
@@ -16,5 +16,28 @@ namespace BankProject
         {
             InitializeComponent();
         }
+        private void checkTextBoxEmpty(object sender, CancelEventArgs e,int numberChar,string message)
+        {
+            if(((TextBox)sender).Text.Length<=numberChar)
+            {
+                e.Cancel = true;
+                epRed.SetError((TextBox)sender, message);
+            }
+            else
+            {
+                e.Cancel = false;
+                epRed.SetError((TextBox)sender, "");
+            }
+        }
+
+        private void tbPassword_Validating(object sender, CancelEventArgs e)
+        {
+            checkTextBoxEmpty(sender, e,8,"password is in [8,16]");
+        }
+
+        private void tbUserName_Validating(object sender, CancelEventArgs e)
+        {
+            checkTextBoxEmpty(sender, e, 0, "Non empty");
+        }
     }
 }
